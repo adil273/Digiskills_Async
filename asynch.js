59ced21756c4a0fb7a16efa2b64944b1e2201a31
adil273 <adil.uet11@gmail.com>
2026-08-31 20:38:57 +0500

    2nd commit

--- asynch.js
+++ asynch.js
@@ -7,14 +7,41 @@
 
 // console.log("Stop");
 
-//Call back function Example
+//function Example-Arrow and Normal Function
 
-const PakStates = (first,second,third,fourth)=>{
-    console.log("Pakistan have following states " + first+ " "+ second+ " " + third+ " " +fourth);
-}
-PakStates("Punjab","Sindh","KPK","Balochistan");
+// const PakStates = (first,second,third,fourth,PunjabCities)=>{
+//     console.log("Pakistan have following states " + first+ " "+ second+ " " + third+ " " +fourth);
+// }
+// PakStates("Punjab","Sindh","KPK","Balochistan");
 
-function PunjabCities(one,two,three){
-    console.log("Famous Cities of Punjab are : "+ one+" " + two+" " + three);
+// function PunjabCities(one,two,three){
+//     console.log("Famous Cities of Punjab are : "+ one+" " + two+" " + three);
+// }
+// PunjabCities("Lahore","Gujranwala","Faisalabad");
+
+//Callback examples
+const pakFamousCities=(Capital,Punjab,KPK,Sindh,Balochistan)=>{
+    console.log("Capital of Pakistan is :" + Capital);
+    console.log("Famouscities of Pakistan are :" + Punjab + ":" + Sindh +":"+ KPK +":"+ Balochistan);
+    Punjab();
+    KPK();
+    Sindh();
+    Balochistan();
 }
-PunjabCities("Lahore","Gujranwala","Faisalabad");
+pakFamousCities("Islamabad",
+function Punjab(){
+    console.log("Famous Cities of Punjab are : Lahore, Faisalabad, Gujranwala");
+},
+function KPK(){
+    console.log("Famous Cities of KPK are : Peshawar, Mardan, Abbottabad");
+},
+function Sindh(){
+    console.log("Famous Cities of Sindh are : Karachi, Hyderabad, Sukkur");
+},
+function Balochistan(){
+    console.log("Famous Cities of Balochistan are : Quetta, Gwadar, Khuzdar");
+});
+function India(){
+    console.log("Famous Cities of India are : Delhi, Mumbai, Bangalore");
+}
+India();
