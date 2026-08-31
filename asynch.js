c3e1c4fd36b12710c774dd546af9ac14fe7bc5e3
adil273 <adil.uet11@gmail.com>
2026-08-31 21:16:49 +0500

    3rd  commit

--- asynch.js
+++ asynch.js
@@ -1,25 +1,25 @@
-//Set Time out Example
-// console.log("Start");
+// Set Time out Example
+console.log("Start");
 
-// setTimeout(()=>{
-//     console.log("Loading...");
-// },3000);
+setTimeout(()=>{
+    console.log("Loading...");
+},3000);
 
-// console.log("Stop");
+console.log("Stop");
 
-//function Example-Arrow and Normal Function
+function Example-Arrow and Normal Function
 
-// const PakStates = (first,second,third,fourth,PunjabCities)=>{
-//     console.log("Pakistan have following states " + first+ " "+ second+ " " + third+ " " +fourth);
-// }
-// PakStates("Punjab","Sindh","KPK","Balochistan");
+const PakStates = (first,second,third,fourth,PunjabCities)=>{
+    console.log("Pakistan have following states " + first+ " "+ second+ " " + third+ " " +fourth);
+}
+PakStates("Punjab","Sindh","KPK","Balochistan");
 
-// function PunjabCities(one,two,three){
-//     console.log("Famous Cities of Punjab are : "+ one+" " + two+" " + three);
-// }
-// PunjabCities("Lahore","Gujranwala","Faisalabad");
+function PunjabCities(one,two,three){
+    console.log("Famous Cities of Punjab are : "+ one+" " + two+" " + three);
+}
+PunjabCities("Lahore","Gujranwala","Faisalabad");
 
-//Callback examples
+// Callback examples
 const pakFamousCities=(Capital,Punjab,KPK,Sindh,Balochistan)=>{
     console.log("Capital of Pakistan is :" + Capital);
     console.log("Famouscities of Pakistan are :" + Punjab + ":" + Sindh +":"+ KPK +":"+ Balochistan);
@@ -45,3 +45,38 @@ function India(){
     console.log("Famous Cities of India are : Delhi, Mumbai, Bangalore");
 }
 India();
+
+// Prmoises Examples
+function login(username,password)
+{
+    if(username==="admin" && password==="1234")
+    {
+        console.log("Login Successful");
+    }
+    else
+    {
+        console.log("Login Failed");
+    }
+}
+login("admin","123456");
+
+function login(username,password)
+{
+    return new Promise((resolve,reject)=>{
+        if(username === "admin" && password === "1234")
+        {
+            resolve("Login Successful");
+        }
+        else
+        {
+            reject("Login Failed");
+        }
+    })
+}
+login("admin","1234")
+.then((result)=>{
+    console.log(result);
+})
+.catch((error)=>{
+    console.log(error);
+});
