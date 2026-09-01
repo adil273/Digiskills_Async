5b27c6c6b64cc966819a95fe78ff75a817e41f24
adil273 <adil.uet11@gmail.com>
2026-09-01 21:14:10 +0500

    Await commit

---
+++ asyncAwait.js
@@ -0,0 +1,43 @@
+// 1ST TYPE
+function sayHello(){
+    console.log("Hello");
+}
+sayHello();
+
+// // 2ND TYPE
+function sayHello1(){
+    return "Hello1";
+}
+const Hello1=sayHello1();
+console.log(Hello1);
+
+// 3RD TYPE
+async function sayHello2(){
+    return "Hello2";
+}
+sayHello2()
+.then((msg) => console.log(msg))
+.catch((err) => console.log(err));
+
+// 4th Type
+async function sayHello3(){
+    return "Hello3";
+}
+const data = await sayHello3();
+console.log(data);
+
+function getUser()
+{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("User Loaded");
+        },3000)
+    })
+}
+async   function loadUser(){
+    const data = await getUser();
+    console.log(data);
+}
+console.log("1");
+loadUser();
+console.log("2");
